a54da08d17a05b6e214c12955c91870b969f645d
igor.asutp@gmail.com <igorasutp>
2021-11-09 05:25:44 +0200

    Label1

--- pmain.dfm
+++ pmain.dfm
@@ -14,6 +14,13 @@ object Fpmain: TFpmain
   OldCreateOrder = False
   PixelsPerInch = 96
   TextHeight = 13
+  object Label1: TLabel
+    Left = 424
+    Top = 40
+    Width = 31
+    Height = 13
+    Caption = 'Label1'
+  end
   object MainMenu1: TMainMenu
     Left = 120
     Top = 32
@@ -28,6 +35,10 @@ object Fpmain: TFpmain
     end
     object N2: TMenuItem
       Caption = #1054#1090#1095#1077#1090#1099
+      object L11: TMenuItem
+        Caption = 'L1'
+        OnClick = L11Click
+      end
     end
   end
 end
--- pmain.pas
+++ pmain.pas
@@ -4,7 +4,7 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.Menus;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.Menus, Vcl.StdCtrls;
 
 type
   TFpmain = class(TForm)
@@ -13,6 +13,9 @@ type
     N2: TMenuItem;
     p11: TMenuItem;
     N21: TMenuItem;
+    Label1: TLabel;
+    L11: TMenuItem;
+    procedure L11Click(Sender: TObject);
   private
     { Private declarations }
   public
@@ -25,5 +28,10 @@ var
 implementation
 
 {$R *.dfm}
+
+procedure TFpmain.L11Click(Sender: TObject);
+begin
+ Label1.Caption:='git';
+end;
 
 end.
